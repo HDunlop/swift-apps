42568154e9f93cdaa28f04f0393fa129a018a2c9
Hugo Dunlop <Dunlop.H@etoncollege.org.uk>
2019-11-26 10:23:39 +0000

    Initial commit of empty tamagotchi project

---
+++ Tamagotchi/Tamagotchi/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tamagotchi
+//
+//  Created by Dunlop, Hugo (PEPW) on 26/11/2019.
+//  Copyright © 2019 Dunlop, Hugo (PEPW). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
