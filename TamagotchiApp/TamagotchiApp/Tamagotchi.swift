2562a607a2bcc776d58001f3ba4d472e9917b2d9
Hugo Dunlop <Dunlop.H@etoncollege.org.uk>
2020-01-21 11:21:26 +0000

    after tues div lesson. Didn't accomplish much. All in all, pretty successful

--- TamagotchiApp/TamagotchiApp/Tamagotchi.swift
+++ TamagotchiApp/TamagotchiApp/Tamagotchi.swift
@@ -12,7 +12,7 @@ class Tamagotchi {
 
     var age: Int = 0
     var weight: Int = 0
-    var discipline: Int = 0
+    var discipline: Int = 50
     var hunger: Int = 0
     var happiness: Int = 100
     var name: String
@@ -38,39 +38,69 @@ class Tamagotchi {
         """)
     }
     
-    func eat() {
-        self.weight += 5
-        self.hunger -= 20
-        self.happiness += 20
+    func doMaths(control: Int, operand: String, value: Int) -> Int {
+        if operand == "+" {
+            if control + value > 100 {
+                return 100
+            } else {
+                return (control + value)
+            }
+        } else {
+            if control - value < 0 {
+                return 0
+            } else {
+                return (control - value)
+            }
+        }
+    }
+    
+    func eatMeal() {
+        self.weight += 3
+        self.hunger = doMaths(control: self.hunger, operand: "-", value: 10)
+        self.happiness = doMaths(control: self.happiness, operand: "+", value: 10)
         self.needsAttention = false
-        self.health -= 10
+        self.health = doMaths(control: self.health, operand: "-", value: 5)
     }
-
+    
+    func eatSnack() {
+        self.weight += 1
+        self.hunger = doMaths(control: self.hunger, operand: "-", value: 5)
+        self.happiness = doMaths(control: self.happiness, operand: "+", value: 6)
+        self.needsAttention = false
+        self.health = doMaths(control: self.health, operand: "-", value: 5)
+    }
+    
     func train() {
-        self.discipline += 20
-        self.happiness -= 15
+        self.discipline = doMaths(control: self.discipline, operand: "+", value: 20)
+        self.happiness = doMaths(control: self.happiness, operand: "-", value: 15)
     }
-
+    
     func giveMedicine() {
-        self.health += 20
-        self.happiness -= 15
+        self.health = doMaths(control: self.health, operand: "+", value: 20)
+        self.happiness = doMaths(control: self.happiness, operand: "-", value: 15)
     }
     
     func playGame() {
-        self.happiness += 30
-        self.hunger += 20
-        self.weight -= 2
-        self.health += 15
+        self.happiness = doMaths(control: self.happiness, operand: "+", value: 30)
+        self.hunger = doMaths(control: self.hunger, operand: "+", value: 20)
+        if self.weight - 2 < 0 {
+            self.weight = 0
+        } else {
+            self.weight -= 2
+        }
+        self.needsAttention = false
+        self.health = doMaths(control: self.health, operand: "+", value: 15)
+        self.discipline = doMaths(control: self.discipline, operand: "-", value: 15)
     }
     
-    func setAge(age: Int) {
-        self.age = age
+    func addAge() {
+        self.age += 1
     }
     
     func notDead() -> Bool {
         if happiness == 0 {
             return true
-        } else if weight == 100 {
+        } else if (weight == 100) || (weight == 0) {
             return true
         } else if health == 0 {
             return true
@@ -80,4 +110,18 @@ class Tamagotchi {
     }
     
     func killedYou() -> Bool {
+        if discipline == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func isItSick() -> Bool {
+        if health == 30 {
+            return true
+        } else {
+            return false
+        }
+    }
 }
--- TamagotchiApp/TamagotchiApp/ViewController.swift
+++ TamagotchiApp/TamagotchiApp/ViewController.swift
@@ -11,23 +11,61 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var statsLabel: UILabel!
+    @IBOutlet var timeRemainingLabel: UILabel!
+    @IBOutlet var feedMealOutlet: UIButton!
+    @IBOutlet var beatOutlet: UIButton!
+    @IBOutlet var giveMedicineOutlet: UIButton!
+    @IBOutlet var playOutlet: UIButton!
+    @IBOutlet var vomitOutlet: UIButton!
+    @IBOutlet var feedSnackOutlet: UIButton!
     
     
     let tamagotchi = Tamagotchi(name: "Sbeve")
-  
+    var timeRemaining = 300
+    var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         updateDisplay()
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countdown), userInfo: nil, repeats: true)
+        timeRemainingLabel.text = "Time Remaining: \(timeRemaining)"
     }
     
     func updateDisplay() {
         statsLabel.text = tamagotchi.displayStats()
     }
     
-    @IBAction func feedButton(_ sender: Any) {
-        tamagotchi.eat()
+    func disableEverything() {
+        feedMealOutlet.isEnabled = false
+        feedSnackOutlet.isEnabled = false
+        beatOutlet.isEnabled = false
+        giveMedicineOutlet.isEnabled = false
+        playOutlet.isEnabled = false
+    }
+    
+    func getSick() {
+        let sick = tamagotchi.isItSick()
+        if sick == true {
+            vomitOutlet.isHidden = false
+        }
+    }
+    
+//    func goToBathroom() {
+//
+//    }
+    
+    @IBAction func vomitButton(_ sender: Any) {
+        vomitOutlet.isHidden = true
+    }
+    
+    @IBAction func feedSnackButton(_ sender: Any) {
+        tamagotchi.eatSnack()
+        updateDisplay()
+    }
+    
+    @IBAction func feedMealButton(_ sender: Any) {
+        tamagotchi.eatMeal()
         updateDisplay()
     }
     
@@ -46,8 +84,25 @@ class ViewController: UIViewController {
         updateDisplay()
     }
     
-    func checkNotDead() {
-        
+//    func checkPlayerNotDead() {
+//        let dead = tamagotchi.killedYou()
+//        if dead == true {
+//
+//        }
+//    }
+    
+    @objc func countdown() {
+        if timeRemaining > 0 {
+            timeRemaining -= 1
+            if timeRemaining % 10 == 0 {
+                tamagotchi.addAge()
+            }
+            
+            timeRemainingLabel.text = "Time Remaining: \(timeRemaining)"
+        } else {
+            timer?.invalidate()
+            disableEverything()
+        }
     }
 }
 
