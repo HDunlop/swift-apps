3b992049316c094807d5aca07e361a439475f1e1
HDunlop <Dunlop.H@etoncollege.org.uk>
2020-01-29 18:47:35 +0000

    initialisation

---
+++ AbsenceRecorder/AbsenceRecorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AbsenceRecorder
+//
+//  Created by ManLikeMe on 29/01/2020.
+//  Copyright © 2020 Hugo Dunlop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
