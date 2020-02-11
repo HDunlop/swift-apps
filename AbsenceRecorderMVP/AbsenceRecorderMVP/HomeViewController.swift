ac2f6fb71acf1cff0ab2948aabbb6c13bc657743
Hugo Dunlop <Dunlop.H@etoncollege.org.uk>
2020-02-11 10:29:20 +0000

    Refactoring names

---
+++ AbsenceRecorderMVP/AbsenceRecorderMVP/HomeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  AbsenceRecorderMVP
+//
+//  Created by Dunlop, Hugo (PEPW) on 11/02/2020.
+//  Copyright © 2020 Dunlop, Hugo (PEPW). All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UITableViewController {
+    
+    var divisions = [Division]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setDivisions()
+        tableView.reloadData()
+    }
+    
+    func setDivisions() {
+        divisions.append(Division(code: "vCW-1", teacher: "Mr. Cormell"))
+        divisions.append(Division(code: "pCX-1", teacher: "Mr. Mann"))
+        divisions.append(Division(code: "MCV3-3 Pure", teacher: "Mrs. Squires-Parkin"))
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return divisions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Division", for: indexPath)
+        
+        let selectedDivision = divisions[indexPath.row]
+        cell.textLabel?.text = selectedDivision.code
+        cell.detailTextLabel?.text = selectedDivision.teacher
+        
+        return cell
+    }
+
+}
+
