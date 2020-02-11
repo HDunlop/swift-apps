2c80225a3076494f8fd4ba23d8b8db4b1adc2547
Hugo Dunlop <Dunlop.H@etoncollege.org.uk>
2020-02-11 10:39:32 +0000

    initial commit for the third time

---
+++ CommentCardWriter/CommentCardWriter/Division.swift
@@ -0,0 +1,21 @@
+//
+//  Division.swift
+//  CommentCardWriter
+//
+//  Created by Dunlop, Hugo (PEPW) on 11/02/2020.
+//  Copyright © 2020 Dunlop, Hugo (PEPW). All rights reserved.
+//
+
+import Foundation
+
+class Division {
+    
+    var code: String
+    var teacher: String
+    var comment: Comment = ""
+    
+    init(code: String, teacher: String) {
+        self.code = code
+        self.teacher = teacher
+    }
+}
