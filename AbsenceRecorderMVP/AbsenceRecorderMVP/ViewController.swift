4dd2f32195a68099b3cdb9e102aefa3694509439
Hugo Dunlop <Dunlop.H@etoncollege.org.uk>
2020-02-11 10:05:17 +0000

    initial commit

---
+++ AbsenceRecorderMVP/AbsenceRecorderMVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AbsenceRecorderMVP
+//
+//  Created by Dunlop, Hugo (PEPW) on 11/02/2020.
+//  Copyright © 2020 Dunlop, Hugo (PEPW). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
